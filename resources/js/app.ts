83e4eac04b16b493cfe86638f93d442871f4c781
Miguel E Uicab Canabal <devmigueluicab@gmail.com>
2024-10-31 05:26:41 -0600

    Integrate PrimeVue

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,40 +1,39 @@
-import '../css/app.css';
-import './bootstrap';
+import "../css/app.css";
+import "./bootstrap";
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, DefineComponent, h } from 'vue';
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createApp, DefineComponent, h } from "vue";
 import PrimeVue from "primevue/config";
-import InputText from 'primevue/inputtext';
-import Textarea from 'primevue/textarea';
-import Dropdown from 'primevue/dropdown';
-import Toast from 'primevue/toast';
-import ToastService from 'primevue/toastservice';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import theme from "tailwindcss/defaultTheme";
+import InputText from "primevue/inputtext";
+import Textarea from "primevue/textarea";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import Timeline from "primevue/timeline";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
+            import.meta.glob<DefineComponent>("./Pages/**/*.vue"),
         ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(PrimeVue, {theme: 'none'})
+            .use(PrimeVue, { theme: "none" })
             .use(ToastService)
-            .component('InputText', InputText)
-            .component('Textarea', Textarea)
-            .component('Dropdown', Dropdown)
-            .component('Toast', Toast)
+            .component("InputText", InputText)
+            .component("Textarea", Textarea)
+            .component("Timeline", Timeline)
+            .component("Toast", Toast)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });

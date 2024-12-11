3ac5111f76dc0ce5af030d4569efd33537e2b237
MIGUEL MORT <devmigueluicab@gmail.com>
2024-12-10 22:59:47 -0600

    MANIFEST.JSON

---
+++ public/build/assets/Articles-Dxt53PJ-.js
@@ -0,0 +1,1 @@
+import{T as t}from"./TitleSection-CBHw1lmm.js";import{D as o,o as a,f as n,a as s,w as r,d as i}from"./app-CCVeHdlT.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const c={class:"container-landing-page"},f=o({__name:"Articles",setup(l){return(p,e)=>(a(),n("section",c,[s(t,null,{default:r(()=>e[0]||(e[0]=[i("Blog")])),_:1})]))}});export{f as default};

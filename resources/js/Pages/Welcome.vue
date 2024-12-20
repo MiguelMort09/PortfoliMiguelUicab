5d573a058bd23642b3838913debefcc3f0198afe
MIGUEL MORT <devmigueluicab@gmail.com>
2024-12-19 19:55:08 -0600

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,6 +2,8 @@
 import {Head} from "@inertiajs/vue3";
 import Hero from "@/Pages/Welcome/Hero.vue";
 import AboutMe from "@/Pages/Welcome/AboutMe.vue";
+import Skills from "@/Pages/Welcome/Skills.vue";
+import Employments from "@/Pages/Welcome/Employments.vue";
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 </script>
 
@@ -10,12 +12,13 @@ import GuestLayout from "@/Layouts/GuestLayout.vue";
     <GuestLayout>
         <Hero/>
         <section class="py-20">
-
+            <Employments/>
         </section>
         <section class="py-10">
             <AboutMe/>
         </section>
         <section class="py-10">
+            <Skills/>
         </section>
     </GuestLayout>
 </template>

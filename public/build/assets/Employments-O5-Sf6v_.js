32e5a74ee2e4bd7a62b462faca6e9e49b3e319ec
MIGUEL MORT <devmigueluicab@gmail.com>
2024-12-19 19:55:15 -0600

    build

---
+++ public/build/assets/Employments-O5-Sf6v_.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Employments.vue_vue_type_script_setup_true_lang-BaJ9pbpK.js";import"./TitleSection-C1Eg4mIa.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./app-QyymLnyc.js";export{o as default};
---
+++ public/build/assets/Skills-XvdITj1j.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Skills.vue_vue_type_script_setup_true_lang-CqAghqs0.js";import"./TitleSection-C1Eg4mIa.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./app-QyymLnyc.js";export{o as default};

75422d0f0ca3413d0a5fbbc34a7a9e97fc3b1e11
MIGUEL MORT <devmigueluicab@gmail.com>
2024-12-19 20:08:54 -0600

    Update ssr.js

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,9 +1,8 @@
-import { createInertiaApp } from '@inertiajs/vue3';
+import {createSSRApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
-import { renderToString } from '@vue/server-renderer';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createSSRApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import {renderToString} from '@vue/server-renderer';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,13 +16,10 @@ createServer((page) =>
                 `./Pages/${name}.vue`,
                 import.meta.glob('./Pages/**/*.vue'),
             ),
-        setup({ App, props, plugin }) {
-            return createSSRApp({ render: () => h(App, props) })
+        setup({App, props, plugin}) {
+            return createSSRApp({render: () => h(App, props)})
                 .use(plugin)
-                .use(ZiggyVue, {
-                    ...page.props.ziggy,
-                    location: new URL(page.props.ziggy.location),
-                });
+                .mount(el)
         },
     }),
 );

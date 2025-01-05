4080c758443615e058bb77a50323e321a92fb825
MIGUEL MORT <devmigueluicab@gmail.com>
2025-01-05 14:11:16 -0600

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,17 +21,6 @@ import {Head} from '@inertiajs/vue3';
                         You're logged in!
                     </div>
                 </div>
-                <Card>
-                    <template #title>Simple Card</template>
-                    <template #content>
-                        <p class="m-0">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
-                            repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa
-                            ratione quam perferendis esse, cupiditate neque
-                            quas!
-                        </p>
-                    </template>
-                </Card>
             </div>
         </div>
     </AuthenticatedLayout>

8be0545eafc5312a006101dbb04f35fd0cd60146
MIGUEL MORT <devmigueluicab@gmail.com>
2025-01-05 20:35:46 -0600

    i´daye

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\EmploymentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TechnologiesController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Socialite\Facades\Socialite;
@@ -10,6 +12,30 @@ use Laravel\Socialite\Facades\Socialite;
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('welcome');
+
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+})->name('auth.redirect');
+
+Route::get('/auth/callback', function () {
+    $githubUser = Socialite::driver('google')->user();
+    $user = \App\Models\User::where('email', $githubUser->getEmail())->first();
+
+    if ($user === null) {
+        $user = \App\Models\User::create([
+            'name' => $githubUser->getName(),
+            'email' => $githubUser->getEmail(),
+            'password' => Hash::make('password'),
+        ]);
+    }
+
+    Auth::login($user);
+
+    return redirect()->route('dashboard');
+
+})->name('auth.callback');
+
 Route::get('/employments', [EmploymentController::class, 'index'])->name('employments.index');
 Route::get('technologies', [TechnologiesController::class, 'index'])->name('technologies.index');
 
@@ -23,27 +49,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/auth/redirect', function () {
-    return Socialite::driver('google')->redirect();
-})->name('auth.redirect');
 
-Route::get('/auth/callback', function () {
-    $githubUser = Socialite::driver('google')->user();
-
-
-    $user = \App\Models\User::where('email', $githubUser->getEmail())->first();
-
-    if ($user === null) {
-        $user = \App\Models\User::create([
-            'name' => $githubUser->getName(),
-            'email' => $githubUser->getEmail(),
-            'password' => \Illuminate\Support\Facades\Hash::make('password'),
-        ]);
-    }
-
-    \Illuminate\Support\Facades\Auth::login($user);
-
-    return redirect()->route('dashboard');
-
-})->name('auth.callback');
 require __DIR__ . '/auth.php';
